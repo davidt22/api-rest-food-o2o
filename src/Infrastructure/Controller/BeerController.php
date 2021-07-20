54ffb0604f165edb4f359790d4cb9bdd3c527b80
David Teruel <d_teruel@hotmail.com>
2021-07-20 13:13:16 +0200

    Add Guzzle http client, created class to manage requests, add docker to run project in local

--- src/Infrastructure/Controller/BeerController.php
+++ src/Infrastructure/Controller/BeerController.php
@@ -6,8 +6,10 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController
 {
-    public function demo(): JsonResponse
+    public function demo(string $name): JsonResponse
     {
+        var_dump($name);
+        die;
         return new JsonResponse('Hola');
     }
 }

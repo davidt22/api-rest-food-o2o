37cb466f2ef0084f305da2434048dc600d96677e
David Teruel <d_teruel@hotmail.com>
2021-07-21 11:37:03 +0200

    Added new detailed endpoint, added tests and renamed controller

--- src/Infrastructure/Controller/SearchBeerController.php
+++ src/Infrastructure/Controller/SearchBeerController.php
@@ -3,16 +3,31 @@
 namespace App\Infrastructure\Controller;
 
 use App\Application\SearchByFood\SearchBeersByFoodService;
+use App\Application\SearchByFoodDetail\SearchBeersByFoodDetailService;
 use App\Shared\Domain\Exception\ServiceException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-class BeerController
+class SearchBeerController
 {
     public function getByFood(string $value, SearchBeersByFoodService $searchBeersByFoodService): JsonResponse
     {
         try {
             $result = $searchBeersByFoodService->execute($value);
+        } catch (ServiceException $exception) {
+            return new JsonResponse($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+
+        return new JsonResponse($result);
+    }
+
+    public function getByFoodDetail(
+        string $value,
+        SearchBeersByFoodDetailService $searchBeersByFoodDetailService
+    ): JsonResponse
+    {
+        try {
+            $result = $searchBeersByFoodDetailService->execute($value);
         } catch (ServiceException $exception) {
             return new JsonResponse($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
